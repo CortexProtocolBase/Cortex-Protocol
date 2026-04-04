6a7b3d952f8e13bf9307187997712ad87d64dc0b
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-04 18:24:08 -0400

    feat(dex): create ABI barrel exports

---
+++ src/lib/dex/abis/index.ts
@@ -0,0 +1,8 @@
+export { UNISWAP_ROUTER_ABI } from "./uniswap-router";
+export { UNISWAP_QUOTER_ABI } from "./uniswap-quoter";
+export { UNISWAP_FACTORY_ABI } from "./uniswap-factory";
+export { UNISWAP_POOL_ABI } from "./uniswap-pool";
+export { AERODROME_ROUTER_ABI } from "./aerodrome-router";
+export { AAVE_POOL_ABI } from "./aave-pool";
+export { COMPOUND_COMET_ABI } from "./compound-comet";
+export { ERC20_ABI } from "./erc20";

dd84dc9e49617946004cd6a65452490eb8c1dafe
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 18:05:45 -0400

    test(ui): add Chip component test

---
+++ __tests__/components/common/Chip.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Chip from "@/components/common/Chip";
+describe("Chip", () => { test("renders without crashing", () => { render(<Chip />); }); });

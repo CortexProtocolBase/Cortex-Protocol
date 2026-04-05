67ff4cdbdd95f07599673c125ab7e4561d204bc9
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 19:40:21 -0400

    test(ui): add Dropdown component test

---
+++ __tests__/components/common/Dropdown.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Dropdown from "@/components/common/Dropdown";
+describe("Dropdown", () => { test("renders without crashing", () => { render(<Dropdown />); }); });

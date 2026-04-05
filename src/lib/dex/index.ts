0307cbb66c4346e918784f728de2c7300708bde3
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 04:01:32 -0400

    feat(dex): add DEX module barrel exports

---
+++ src/lib/dex/index.ts
@@ -0,0 +1,8 @@
+export { UniswapV3Adapter, uniswapAdapter } from "./uniswap";
+export { AerodromeAdapter, aerodromeAdapter } from "./aerodrome";
+export { AaveV3Adapter, aaveAdapter } from "./aave";
+export { CompoundV3Adapter, compoundUsdcAdapter, compoundWethAdapter } from "./compound";
+export { DEXRouter, dexRouter } from "./router";
+export * from "./types";
+export * from "./constants";
+export * from "./utils";

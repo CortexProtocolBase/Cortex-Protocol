02e98d0dabd25b4ae51a3aedbb351821c06b2ddd
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 18:52:39 -0400

    feat(sentiment): add sentiment module barrel exports

---
+++ src/lib/sentiment/index.ts
@@ -0,0 +1,7 @@
+export { getFarcasterSentiment, fetchFarcasterCasts } from "./farcaster";
+export { getTwitterSentiment } from "./twitter";
+export { getOnchainSentiment } from "./onchain";
+export { getAggregatedSentiment } from "./aggregator";
+export { scoreSentiment, scoreWithVolume } from "./scorer";
+export { ASSET_KEYWORDS, POSITIVE_TERMS, NEGATIVE_TERMS } from "./keywords";
+export * from "./types";

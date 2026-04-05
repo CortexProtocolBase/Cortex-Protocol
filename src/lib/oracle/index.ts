70bbf6cbaaf27f062ab3fed620468c6b58d459b0
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 10:36:59 -0400

    feat(oracle): add oracle barrel exports

---
+++ src/lib/oracle/index.ts
@@ -0,0 +1,5 @@
+export { getChainlinkPrice, getChainlinkPrices, isPriceFeedHealthy } from "./chainlink";
+export { getCoinGeckoPrice, getCoinGeckoPrices } from "./coingecko";
+export { getAggregatedPrice, getAggregatedPrices, isPriceReliable } from "./aggregator";
+export { CHAINLINK_FEEDS, STALE_PRICE_THRESHOLD, DEVIATION_THRESHOLD } from "./feeds";
+export * from "./types";

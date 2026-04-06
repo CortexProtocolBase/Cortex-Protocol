444661b38fe3b206af7fe13a8fc911ac161bdbf4
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 14:26:26 -0400

    test(ui): add Timeline component test

---
+++ __tests__/components/common/Timeline.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Timeline from "@/components/common/Timeline";
+describe("Timeline", () => { test("renders without crashing", () => { render(<Timeline />); }); });

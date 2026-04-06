75bf3fa57cf4f84e057345987991b0b8c3be99be
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 07:56:18 -0400

    test(ui): add TextArea component test

---
+++ __tests__/components/common/TextArea.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import TextArea from "@/components/common/TextArea";
+describe("TextArea", () => { test("renders without crashing", () => { render(<TextArea />); }); });

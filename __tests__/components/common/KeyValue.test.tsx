6fdfad524d3e34b663a27a28297dbd3923897859
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 18:03:16 -0400

    test(ui): add KeyValue component test

---
+++ __tests__/components/common/KeyValue.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import KeyValue from "@/components/common/KeyValue";
+describe("KeyValue", () => { test("renders without crashing", () => { render(<KeyValue />); }); });

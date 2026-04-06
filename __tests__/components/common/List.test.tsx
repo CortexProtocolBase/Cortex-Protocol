eca206a46f1071da7e8a15a2073b9b4a33af0e4a
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 22:37:27 -0400

    test(ui): add List component test

---
+++ __tests__/components/common/List.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import List from "@/components/common/List";
+describe("List", () => { test("renders without crashing", () => { render(<List />); }); });

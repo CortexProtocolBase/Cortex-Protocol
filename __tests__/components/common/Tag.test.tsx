61e62342f9af477bb4a8796fbe193b1649bb1c25
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 07:05:16 -0400

    test(ui): add Tag component test

---
+++ __tests__/components/common/Tag.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Tag from "@/components/common/Tag";
+describe("Tag", () => { test("renders without crashing", () => { render(<Tag />); }); });

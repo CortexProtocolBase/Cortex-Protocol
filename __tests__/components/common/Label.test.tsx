996976cd405a2a128c5915dd3754256de69aa2de
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 21:00:24 -0400

    test(ui): add Label component test

---
+++ __tests__/components/common/Label.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Label from "@/components/common/Label";
+describe("Label", () => { test("renders without crashing", () => { render(<Label />); }); });

f6f7a9930fb503cb9039e464fb50c8cdf0cda57b
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 06:17:35 -0400

    test(ui): add TabPanel component test

---
+++ __tests__/components/common/TabPanel.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import TabPanel from "@/components/common/TabPanel";
+describe("TabPanel", () => { test("renders without crashing", () => { render(<TabPanel />); }); });

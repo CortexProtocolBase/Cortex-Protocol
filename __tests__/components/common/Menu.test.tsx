096e8239c87e66b74dce2b8aeb2f06f31847670b
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 00:00:42 -0400

    test(ui): add Menu component test

---
+++ __tests__/components/common/Menu.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Menu from "@/components/common/Menu";
+describe("Menu", () => { test("renders without crashing", () => { render(<Menu />); }); });

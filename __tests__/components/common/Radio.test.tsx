4fceace9bd42aba10d5308130937b8331b212aa0
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 02:44:24 -0400

    test(ui): add Radio component test

---
+++ __tests__/components/common/Radio.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Radio from "@/components/common/Radio";
+describe("Radio", () => { test("renders without crashing", () => { render(<Radio />); }); });

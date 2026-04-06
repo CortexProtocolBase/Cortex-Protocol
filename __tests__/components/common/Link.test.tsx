7609574beb8e4b04c8961aad0573ed121cda0bed
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 21:51:42 -0400

    test(ui): add Link component test

---
+++ __tests__/components/common/Link.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Link from "@/components/common/Link";
+describe("Link", () => { test("renders without crashing", () => { render(<Link />); }); });

a681069bdcf4699d079309b0b34f04acb67f3a17
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 04:54:04 -0400

    test(ui): add Switch component test

---
+++ __tests__/components/common/Switch.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Switch from "@/components/common/Switch";
+describe("Switch", () => { test("renders without crashing", () => { render(<Switch />); }); });

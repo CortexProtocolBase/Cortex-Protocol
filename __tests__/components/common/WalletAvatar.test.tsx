9c5f79428c6e28d30396b98ec3872692906c199c
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 10:47:56 -0400

    test(ui): add WalletAvatar component test

---
+++ __tests__/components/common/WalletAvatar.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import WalletAvatar from "@/components/common/WalletAvatar";
+describe("WalletAvatar", () => { test("renders without crashing", () => { render(<WalletAvatar />); }); });

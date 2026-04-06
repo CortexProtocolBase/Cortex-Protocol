191124a720987d7592539ed65ecb94ec1dc909d7
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-05 20:31:16 -0400

    test(ui): add IconButton component test

---
+++ __tests__/components/common/IconButton.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import IconButton from "@/components/common/IconButton";
+describe("IconButton", () => { test("renders without crashing", () => { render(<IconButton />); }); });

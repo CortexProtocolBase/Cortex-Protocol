646964b303ca3fda941949871292e3e029b40671
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 17:04:15 -0400

    test(ui): add InfoRow component test

---
+++ __tests__/components/common/InfoRow.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import InfoRow from "@/components/common/InfoRow";
+describe("InfoRow", () => { test("renders without crashing", () => { render(<InfoRow />); }); });

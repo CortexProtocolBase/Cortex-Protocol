bfb0b34d8998595551d522ab86a15e7bb29b0874
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 02:51:52 -0400

    feat(notifications): add notification module exports

---
+++ src/lib/notifications/index.ts
@@ -0,0 +1,6 @@
+export { dispatch } from "./dispatcher";
+export { sendDiscordNotification } from "./discord";
+export { sendTelegramNotification } from "./telegram";
+export { sendEmailNotification } from "./email";
+export { renderTemplate, TEMPLATES } from "./templates";
+export * from "./types";

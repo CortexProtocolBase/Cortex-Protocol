539166bf14443f149976c8eb3247acb163341051
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 05:37:00 -0400

    test(ui): add Tab component test

---
+++ __tests__/components/common/Tab.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Tab from "@/components/common/Tab";
+describe("Tab", () => { test("renders without crashing", () => { render(<Tab />); }); });

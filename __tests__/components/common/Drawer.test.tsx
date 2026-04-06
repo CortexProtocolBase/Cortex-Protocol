8fa0bc0879bf6313e592eed0213db49dc6711239
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-06 12:09:07 -0400

    test(ui): add Drawer component test

---
+++ __tests__/components/common/Drawer.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import Drawer from "@/components/common/Drawer";
+describe("Drawer", () => { test("renders without crashing", () => { render(<Drawer />); }); });

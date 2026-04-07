d815bc49a795e27b0c8e7ec4687a8ffafc68b23b
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-07 00:58:16 -0400

    test(ui): add VotePower component test

---
+++ __tests__/components/common/VotePower.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import VotePower from "@/components/common/VotePower";
+describe("VotePower", () => { test("renders without crashing", () => { render(<VotePower />); }); });

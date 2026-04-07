7ad597f902361da8467f23578e8c3dd734087d3c
CortexProtocolBase <268478687+CortexProtocolBase@users.noreply.github.com>
2026-04-07 02:22:39 -0400

    test(ui): add RewardDisplay component test

---
+++ __tests__/components/common/RewardDisplay.test.tsx
@@ -0,0 +1,3 @@
+import { render } from "@testing-library/react";
+import RewardDisplay from "@/components/common/RewardDisplay";
+describe("RewardDisplay", () => { test("renders without crashing", () => { render(<RewardDisplay />); }); });
